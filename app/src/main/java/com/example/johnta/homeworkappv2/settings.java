b9224b8b06707e5dbdf54a75ec2d5d758ba92de3
johntaaa <birdmcfly1@gmail.com>
2017-04-06 13:22:26 -0400

    Popup w/ planner V3

--- app/src/main/java/com/example/johnta/homeworkappv2/settings.java
+++ app/src/main/java/com/example/johnta/homeworkappv2/settings.java
@@ -3,13 +3,11 @@ package com.example.johnta.homeworkappv2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class settings extends AppCompatActivity {
+public class Settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-        //test
     }
 }
