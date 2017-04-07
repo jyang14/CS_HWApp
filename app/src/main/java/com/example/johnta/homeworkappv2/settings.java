46d40c16622d48bc4e07cc06712f42e2b8bf897d
celinebui <ctbui@wpi.edu>
2017-04-07 08:42:32 -0400

    Celine

--- app/src/main/java/com/example/johnta/homeworkappv2/settings.java
+++ app/src/main/java/com/example/johnta/homeworkappv2/settings.java
@@ -3,7 +3,7 @@ package com.example.johnta.homeworkappv2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Settings extends AppCompatActivity {
+public class settings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
