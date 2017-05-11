ebec48bb62253db0dd4081387210035d194c2f53
johntaaa <birdmcfly1@gmail.com>
2017-05-11 00:59:59 -0400

    Up to date as of May 11, 12:00 AM

---
+++ app/src/main/java/com/example/johnta/homeworkappv2/popup/CloudPopup.java
@@ -0,0 +1,31 @@
+package com.example.johnta.homeworkappv2.popup;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.DisplayMetrics;
+import android.widget.EditText;
+
+import com.example.johnta.homeworkappv2.R;
+
+public class CloudPopup extends AppCompatActivity {
+
+    private EditText txtInput;
+    private EditText input_assignment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popup_cloud);
+
+        txtInput = (EditText) findViewById(R.id.name_of_class);
+        input_assignment = (EditText) findViewById(R.id.class_assignment);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width = dm.widthPixels;
+        int height = dm.heightPixels;
+
+        getWindow().setLayout((int)(width*0.8),(int)(height*0.6));
+    }
+}
